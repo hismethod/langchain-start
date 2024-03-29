f2f26f746965f14ed6edcf564ec622159b4cadf9
moon <his.methods@gmail.com>
2024-03-29 10:19:26 +0000

    Add Example

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log(process.env.LANGCHAIN_TRACING_V2);
+import { ChatOpenAI } from "@langchain/openai";
+
+const model = new ChatOpenAI({
+  model: "gpt-3.5-turbo",
+  temperature: 0,
+});
+
+const response = await model.invoke("대한민국 국기에 해당하는 이모지는 뭐야?");
+
+console.log(response);
