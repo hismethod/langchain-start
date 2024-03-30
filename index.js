7783a59e0e2a7e485b2b6afbccfecd4ad08e2d3a
moon <his.methods@gmail.com>
2024-03-30 01:40:55 +0000

    Add anthropic llm

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-import { ChatOpenAI } from "@langchain/openai";
+import { ChatAnthropic } from "@langchain/anthropic";
 
-const model = new ChatOpenAI({
-  model: "gpt-3.5-turbo",
-  temperature: 0,
+const model = new ChatAnthropic({
+  modelName: "claude-3-haiku-20240307",
 });
 
 const response = await model.invoke("대한민국 국기에 해당하는 이모지는 뭐야?");
